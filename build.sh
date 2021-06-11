3b587b72a087acd5b8b9b17f73e2f37286265ce8
Joe Previte <jjprevite@gmail.com>
2021-06-11 14:43:11 -0700

    fix: remove parcel and use tsc

--- build.sh
+++ build.sh
@@ -3,14 +3,7 @@ set -euo pipefail
 
 
 main() {
-  parcel build \
-    --public-url "." \
-    --dist-dir dist \
-    --no-optimize \
-    src/browser/register.ts \
-    src/browser/serviceWorker.ts \
-    src/browser/pages/login.ts \
-    src/browser/pages/vscode.ts
+  yarn run tsc
 }
 
 main "$@"
